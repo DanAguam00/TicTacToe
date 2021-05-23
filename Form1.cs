e29702e3bc665cc764a9b531cc021ba733dcd75d
DanAguam00 <82425604+DanAguam00@users.noreply.github.com>
2021-05-23 17:43:46 +0800

    Added interaction to buttons.

--- Form1.cs
+++ Form1.cs
@@ -17,5 +17,102 @@ namespace TicTacToe
             InitializeComponent();
         }
 
+        private void Btn0_Click(object sender, EventArgs e)
+        {
+            Btn0.Text = "X";
+            Btn0.BackColor = Color.Red;
+            Winner();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            Btn1.Text = "X";
+            Winner();
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            Btn2.Text = "X";
+            Winner();
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            Btn3.Text = "X";
+            Winner();
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+            Btn4.Text = "X";
+            Winner();
+        }
+
+        private void Btn5_Click(object sender, EventArgs e)
+        {
+            Btn5.Text = "X";
+            Winner();
+        }
+
+        private void Btn6_Click(object sender, EventArgs e)
+        {
+            Btn6.Text = "X";
+            Winner();
+        }
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+            Btn7.Text = "X";
+            Winner();
+        }
+
+        private void Btn8_Click(object sender, EventArgs e)
+        {
+            Btn8.Text = "X";
+            Winner();
+        }
+
+        private void Winner()
+        {
+            bool win = false;
+            if ((Btn0.Text == Btn1.Text) && (Btn1.Text == Btn2.Text))
+            {
+                win = true;
+            }
+            else if ((Btn3.Text == Btn4.Text) && (Btn4.Text == Btn5.Text))
+            {
+                win = true;
+            }
+            else if ((Btn6.Text == Btn7.Text) && (Btn7.Text == Btn8.Text))
+            {
+                win = true;
+            }
+            else if ((Btn0.Text == Btn3.Text) && (Btn3.Text == Btn6.Text))
+            {
+                win = true;
+            }
+            else if ((Btn1.Text == Btn4.Text) && (Btn4.Text == Btn7.Text))
+            {
+                win = true;
+            }
+            else if ((Btn2.Text == Btn5.Text) && (Btn5.Text == Btn8.Text))
+            {
+                win = true;
+            }
+            else if ((Btn0.Text == Btn4.Text) && (Btn4.Text == Btn8.Text))
+            {
+                win = true;
+            }
+            else if ((Btn2.Text == Btn4.Text) && (Btn4.Text == Btn6.Text))
+            {
+                win = true;
+            }
+
+
+            if (win == true)
+            {
+                MessageBox.Show("Winner");
+            }
+        }
     }
 }
